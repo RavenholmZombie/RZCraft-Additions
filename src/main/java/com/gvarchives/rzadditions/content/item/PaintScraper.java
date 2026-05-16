6b34740371b4fc5b17ce485851d8e77cb61750ae
Christopher <supersoftware1234@gmail.com>
2026-05-16 18:10:03 -0400

    Finished the Paint Scraper

--- src/main/java/com/gvarchives/rzadditions/content/item/PaintScraper.java
+++ src/main/java/com/gvarchives/rzadditions/content/item/PaintScraper.java
@@ -1,11 +1,21 @@
 package com.gvarchives.rzadditions.content.item;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class PaintScraper extends Item
 {
     public PaintScraper(Properties properties)
     {
         super(properties);
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos pos, Player player)
+    {
+        return false;
     }
 }
